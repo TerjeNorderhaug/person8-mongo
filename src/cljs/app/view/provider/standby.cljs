d63cfb36c0bd63389a725a3d265a352e714df8f7
Terje Norderhaug <terje@in-progress.com>
2018-01-20 18:17:18 -0800

    add panes for provider dashboard

---
+++ src/cljs/app/view/provider/standby.cljs
@@ -0,0 +1,36 @@
+(ns app.view.provider.standby
+  (:require
+   [reagent.core :as reagent
+     :refer [atom]]
+   [re-frame.core :as rf]
+   [cljsjs.material-ui]
+   [cljs-react-material-ui.core :as material
+     :refer [get-mui-theme color]]
+   [cljs-react-material-ui.reagent :as ui]
+   [cljs-react-material-ui.icons :as ic]
+   [goog.string :as gstring]
+   [app.view.provider.pane
+     :refer [pane]]))
+
+(defn view [{:keys [waiting] :as session}]
+ [:div.well
+  [:table.table.waiting
+   [:thead [:caption {:style {:width "100%"}}
+            "Waiting Room"]]
+   (into [:tbody]
+         (for [[id {:keys [fname lname avatar
+                           state-of-residency]
+                    :as patient}]
+               (map-indexed vector
+                            (if waiting @waiting))]
+            ^{:key id}
+            [:tr
+             [:td
+              [:img.avatar {:src (str "https://well-api.joinwell.com/"
+                                  avatar)}]]
+             [:td
+               {:style {:text-align "left"}}
+               (str fname lname)]]))]])
+
+(defmethod pane "dashboard" [session]
+  [view session])
