f2d88d1ec1478d203bf334387d00e4f24d052aaf
Terje Norderhaug <terje@in-progress.com>
2018-10-28 15:12:44 -0700

    add change tab menu to app

--- src/universal/app/mobile/toolbar.cljs
+++ src/universal/app/mobile/toolbar.cljs
@@ -11,9 +11,20 @@
    [cljs-react-material-ui.reagent :as ui]
    [cljs-react-material-ui.icons :as ic]))
 
-(defn toolbar [{:keys [brand] :as session}]
-  [ui/app-bar
-   {:on-left-icon-button-touch-tap #()
-    :on-right-icon-button-touch-tap #()
-    :title (reagent/as-element
-            [:div (if brand @brand)])}])
+(defn toolbar [{:as session}]
+  (let [open (atom false)]
+    (fn [{:keys [brand tab] :as session}]
+      [ui/app-bar
+       {:on-left-icon-button-touch-tap #(reset! open true)
+        :on-right-icon-button-touch-tap #()
+        :title (reagent/as-element
+                [:div (if brand @brand)])}
+       (into [ui/drawer {:open @open
+                         :onClose #(reset! open false)}]
+             (for [{:keys [title id] :as item} (:options @tab)]
+               ^{:key id}
+               [ui/menu-item
+                {:on-click #(do
+                              (reset! open false)
+                              (rf/dispatch [:change-tab id]))}
+                title]))])))
